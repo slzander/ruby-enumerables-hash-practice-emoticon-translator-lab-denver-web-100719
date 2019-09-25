94561a14dcb8d66f443699a3b2ddc46bba3dc457
Stacey Zander <staceyzander@gmail.com>
2019-09-25 03:27:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,7 @@ end
 
 def get_japanese_emoticon(file_path = './lib/emoticons.yml', japanese_emoticon)
   library = load_library(file_path)
-  if library 
+  if library
 end
 
 def get_english_meaning
