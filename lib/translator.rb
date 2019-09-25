fbf7e0796f723b3fb599d2843010b41df49b84ba
Stacey Zander <staceyzander@gmail.com>
2019-09-25 03:32:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,8 +21,8 @@ end
 
 def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
   library = load_library(file_path)
-  if library["get_emoticon"].include?(japanese_emoticon)
-    library["get_emoticon"][japanese_emoticon]
+  if library["get_meaning"].include?(japanese_emoticon)
+    library["get_meaning"][japanese_emoticon]
   else
     "Sorry that emoticon was not foud"
   end
