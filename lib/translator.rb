5c095476fb26477e66a118825d922f690c849ced
Stacey Zander <staceyzander@gmail.com>
2019-09-25 03:15:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require "yaml"
 def load_library(file_path)
   file = File.read(file_path)
   emoticons = YAML.load(file)
-  output = {"get_meaning" => {}
+  output = {"get_meaning" => {}, "get_emoticon" =>
 end
 
 def get_japanese_emoticon
